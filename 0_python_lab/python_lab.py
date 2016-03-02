6808a60b0f266241e60b9561ffb6d2c0649d7db1
Jesse Anderson <jesse.anderson@gmail.com>
2016-03-01 21:13:22 -0700

    1st lab

--- 0_python_lab/python_lab.py
+++ 0_python_lab/python_lab.py
@@ -25,12 +25,12 @@ divisible_by_3 = (673 + 909) % 3 == 0
 ## 4: (Task 4) Conditional Expression
 x = -9
 y = 1/2
-expression_val = 1
+expression_val = 2**(y+1/2) if x+10<0 else 2**(y-1/2)
 
 
 
 ## 5: (Task 5) Squares Set Comprehension
-first_five_squares = { x**x for x in {1,2,3,4,5} }
+first_five_squares = { x**2 for x in {1,2,3,4,5} }
 
 
 
@@ -43,8 +43,8 @@ first_five_pows_two = { 2**x for x in {0,1,2,3,4} }
 # Assign three-element sets to X1 and Y1 so that
 # {x*y for x in X1 for y in Y1} evaluates to a nine-element set.
 
-X1 = { ..., ..., ... }
-Y1 = { ..., ..., ... }
+X1 = { 2, 3, 4}
+Y1 = { 5, 6, 7}
 
 
 
@@ -52,8 +52,8 @@ Y1 = { ..., ..., ... }
 # Assign disjoint three-element sets to X1 and Y1 so that
 # {x*y for x in X1 for y in Y1} evaluates to a five-element set.
 
-X2 = { ..., ..., ... }
-Y2 = { ..., ..., ... }
+X2 = { 2, 4, 8}
+Y2 = { 5, 10, 20}
 
 
 
@@ -61,7 +61,7 @@ Y2 = { ..., ..., ... }
 S = {1, 2, 3, 4}
 T = {3, 4, 5, 6}
 # Replace { ... } with a one-line set comprehension that evaluates to the intersection of S and T
-S_intersect_T = { ... }
+S_intersect_T = { x for x in S if x in T}
 
 
 
@@ -70,13 +70,13 @@ list_of_numbers = [20, 10, 15, 75]
 # Replace ... with a one-line expression that evaluates to the average of list_of_numbers.
 # Your expression should refer to the variable list_of_numbers, and should work
 # for a list of any length greater than zero.
-list_average = ... 
+list_average = sum(list_of_numbers) / len(list_of_numbers)
 
 
 
 ## 11: (Task 11) Cartesian-product comprehension
 # Replace ... with a double list comprehension over ['A','B','C'] and [1,2,3]
-cartesian_product = ...
+cartesian_product = [[a,b] for a in ['A','B','C'] for b in [1,2,3]]
 
 
 
@@ -84,48 +84,48 @@ cartesian_product = ...
 LofL = [[.25, .75, .1], [-1, 0], [4, 4, 4, 4]]
 # Replace ... with a one-line expression of the form sum([sum(...) ... ]) that
 # includes a comprehension and evaluates to the sum of all numbers in all the lists.
-LofL_sum = ...
+LofL_sum = sum([sum(l) for l in LofL])
 
 
 
 ## 13: (Task 13) Three-element tuples summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace [ ... ] with a one-line list comprehension in which S appears
-zero_sum_list = [ ... ] 
+zero_sum_list = [(i,j,k) for i in S for j in S for k in S if sum((i,j,k)) == 0]
 
 
 
 ## 14: (Task 14) Nontrivial three-element tuples summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace [ ... ] with a one-line list comprehension in which S appears
-exclude_zero_list = [ ... ]
+exclude_zero_list = [(i,j,k) for i in S for j in S for k in S if sum((i,j,k)) == 0 and (i,j,k) != (0,0,0)]
 
 
 
 ## 15: (Task 15) One nontrivial three-element tuple summing to zero
 S = {-4, -2, 1, 2, 5, 0}
 # Replace ... with a one-line expression that uses a list comprehension in which S appears
-first_of_tuples_list = ...
-
+first_of_tuples_list = [(i,j,k) for i in S for j in S for k in S if sum((i,j,k)) == 0 and (i,j,k) != (0,0,0)][0]
 
 
 ## 16: (Task 16) List and set differ
 # Assign to example_L a list such that len(example_L) != len(list(set(example_L)))
-example_L = [...]
+example_L = [1, 2, 1]
 
 
 
 ## 17: (Task 17) Odd numbers
 # Replace {...} with a one-line set comprehension over a range of the form range(n)
-odd_num_list_range = {...}
+odd_num_list_range = {x for x in range(100) if x % 2 ==1 }
 
 
 
 ## 18: (Task 18) Using range and zip
-# In the line below, replace ... with an expression that does not include a comprehension.
+# In the line below, replace ... with an expression that does not include a comprehension
 # Instead, it should use zip and range.
 # Note: zip() does not return a list. It returns an 'iterator of tuples'
-range_and_zip = ...
+L = ['A','B','C','D','E']
+range_and_zip = list(zip(range(5), L))
 
 
 
@@ -135,7 +135,7 @@ B = [1, 15, 20]
 # Replace [...] with a one-line comprehension that uses zip together with the variables A and B.
 # The comprehension should evaluate to a list whose ith element is the ith element of
 # A plus the ith element of B.
-list_sum_zip = [...]
+list_sum_zip = [a + b for (a,b) in zip(A,B)]
 
 
 
@@ -144,7 +144,7 @@ dlist = [{'James':'Sean', 'director':'Terence'}, {'James':'Roger', 'director':'L
 k = 'James'
 # Replace [...] with a one-line comprehension that uses dlist and k
 # and that evaluates to ['Sean','Roger','Pierce']
-value_list = [...]
+value_list = [d[k] for d in dlist]
 
 
 
@@ -152,22 +152,22 @@ value_list = [...]
 dlist = [{'Bilbo':'Ian','Frodo':'Elijah'},{'Bilbo':'Martin','Thorin':'Richard'}]
 k = 'Bilbo'
 #Replace [...] with a one-line comprehension 
-value_list_modified_1 = [...] # <-- Use the same expression here
+value_list_modified_1 = [d.get(k, 'NOT PRESENT') for d in dlist] # <-- Use the same expression here
 k = 'Frodo'
-value_list_modified_2 = [...] # <-- as you do here
+value_list_modified_2 = [d.get(k, 'NOT PRESENT') for d in dlist] # <-- as you do here
 
 
 
 ## 22: (Task 22) A dictionary mapping integers to their squares
 # Replace {...} with a one-line dictionary comprehension
-square_dict = {...}
+square_dict = {x:x**2 for x in range(100)}
 
 
 
 ## 23: (Task 23) Making the identity function
 D = {'red','white','blue'}
 # Replace {...} with a one-line dictionary comprehension
-identity_dict = {...}
+identity_dict = {d:d for d in D}
 
 
 
@@ -177,27 +177,26 @@ digits = set(range(base))
 # Replace { ... } with a one-line dictionary comprehension
 # Your comprehension should use the variables 'base' and 'digits' so it will work correctly if these
 # are assigned different values (e.g. base = 2 and digits = {0,1})
-representation_dict = { ... }
-
+representation_dict = {i*base**2 + j*base**1 + k*base**0:(i,j,k) for i in digits for j in digits for k in digits}
 
 
 ## 25: (Task 25) A dictionary mapping names to salaries
 id2salary = {0:1000.0, 1:1200.50, 2:990}
 names = ['Larry', 'Curly', 'Moe']
 # Replace { ... } with a one-line dictionary comprehension that uses id2salary and names.
-listdict2dict = { ... }
+listdict2dict = {names[k]:v for (k,v) in id2salary.items()}
 
 
 
 ## 26: (Task 26) Procedure nextInts
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
-def nextInts(L): return [ ... ]
+def nextInts(L): return [i + 1 for i in L]
 
 
 
 ## 27: (Task 27) Procedure cubes
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
-def cubes(L): return [ ... ] 
+def cubes(L): return [ i**3 for i in L ] 
 
 
 
@@ -206,7 +205,7 @@ def cubes(L): return [ ... ]
 # Output: the list L such that L[i] is the value associated in dct with keylist[i]
 # Example: dict2list({'a':'A', 'b':'B', 'c':'C'},['b','c','a']) should equal ['B','C','A']
 # Complete the procedure definition by replacing [ ... ] with a one-line list comprehension
-def dict2list(dct, keylist): return [ ... ]
+def dict2list(dct, keylist): return [dct[k] for k in keylist ]
 
 
 
@@ -215,5 +214,5 @@ def dict2list(dct, keylist): return [ ... ]
 # Output: the dictionary that maps keylist[i] to L[i] for i=0,1,...len(L)-1
 # Example: list2dict(['A','B','C'],['a','b','c']) should equal {'a':'A', 'b':'B', 'c':'C'}
 # Complete the procedure definition by replacing { ... } with a one-line dictionary comprehension
-def list2dict(L, keylist): return { ... }
+def list2dict(L, keylist): return dict(zip(keylist, L))
 
