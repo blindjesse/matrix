683c40799861f9706e24b33675114ac1c6f8100e
Jesse Anderson <jesse.anderson@gmail.com>
2016-03-05 14:24:38 -0700

    All zero probs done

--- 0_The_function_problems/The_Function_problems.py
+++ 0_The_function_problems/The_Function_problems.py
@@ -22,7 +22,9 @@ def tuple_sum(A, B):
     >>> tuple_sum([(0,1),(-1,0),(2,2)], [(3,4),(5,6),(7,8)])
     [(3, 5), (4, 6), (9, 10)]
     '''
-    pass
+    A = list(enumerate(A))
+    B = list(enumerate(B))
+    return [(x[1][0] + B[x[0]][1][0], x[1][1] + B[x[0]][1][1]) for x in A]
 
 
 
@@ -37,7 +39,7 @@ def inv_dict(d):
     Example:
     >>> inv_dict({'goodbye':  'au revoir', 'thank you': 'merci'}) == {'merci':'thank you', 'au revoir':'goodbye'}
     '''
-    pass
+    return {v:k for (k,v) in d.items()}
 
 
 
@@ -53,21 +55,20 @@ def row(p, n):
     >>> row(10,4)
     [10, 11, 12, 13]
     '''
-    pass
+    return [p + i for i in range(n)]
 
-comprehension_with_row = ...
-
-comprehension_without_row = ...
+comprehension_with_row = [row(p, 20) for p in range(15)]
+comprehension_without_row = [[p + i for i in range(20)] for p in range(15)]
 
 
 
 ## 4: (Problem 4) Probability Exercise 1
-Pr_f_is_even = ...
-Pr_f_is_odd  = ...
+Pr_f_is_even = 0.7
+Pr_f_is_odd  = 0.3
 
 
 
 ## 5: (Problem 5) Probability Exercise 2
-Pr_g_is_1    = ...
-Pr_g_is_0or2 = ...
+Pr_g_is_1    = 0.4
+Pr_g_is_0or2 = 0.6
 
